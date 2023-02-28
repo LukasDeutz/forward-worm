7281e9d770a68931afa5d304302ad5e5336d44ed
amoghasiddhi <amoghasiddhi@amoghasiddhi-ThinkPad-T440>
2023-02-28 12:33:07 +0000

    Merge updates from mju

--- forward_worm/coil/continuous_coil.py
+++ forward_worm/coil/continuous_coil.py
@@ -3,26 +3,18 @@ Created on 10 Feb 2023
 
 @author: lukas
 '''
-
-#Built-in imports
-from os.path import join
-
 #Third-party imports
 import numpy as np
-import h5py
 
 # Local imports
 from forward_worm.coil.coil_dirs import log_dir, sim_dir, sweep_dir
 from forward_worm.util import simulate_experiments, save_experiment_to_h5, sim_exp_wrapper
 
 from simple_worm_experiments.coiling.coiling import CoilingExperiment
-from simple_worm_experiments.grid_loader import GridLoader, GridPoolLoader
 from simple_worm_experiments.experiment_post_processor import EPP
 from simple_worm_experiments.model_parameter import default_model_parameter
 
 from parameter_scan import ParameterGrid
-from parameter_scan.util import load_grid_param, load_file_grid
-from mp_progress_logger import FWProgressLogger 
 
 #------------------------------------------------------------------------------ 
 # Parameter
@@ -59,6 +51,8 @@ def continuous_coil_parameter():
     parameter['Ds_lr_t'] = 1.0/32
     parameter['s0_lr_t'] = 0.25
 
+    return parameter
+
 #------------------------------------------------------------------------------ 
 # Post processing
 
@@ -70,7 +64,7 @@ def compute_roll_frequency_from_euler_angle(h5, PG):
 
     # roll angle
     alpha_sweep = h5['FS']['theta'][:, :, 0, :]            
-    t = h5['times'][:]
+    t = h5['t'][:]
 
     s_arr = np.linspace(0, 1, PG.base_parameter['N'])
     s_mask = s_arr >= PG.base_parameter['s0_lr_t']
@@ -94,13 +88,15 @@ def compute_roll_frequency_from_euler_angle(h5, PG):
     else: h5.create_dataset('f_R_std', data = f_R_std_arr)
     
     return
+    
+#------------------------------------------------------------------------------ 
+# Simualtions        
             
 def sim_coil_A(N_worker,
         simulate = True,
         save_raw_data = True,
         overwrite = False,
         debug = False):
-
     '''
     Parameter sweep over curvature amplitude
     '''
@@ -109,12 +105,12 @@ def sim_coil_A(N_worker,
     param['T'] = 20.0
     
     # Parameter Grid    
-    A_min, A_max, A_step = 1.0, 8.01, 1.0
+    A_min, A_max, A_step = 1.0, 4.01, 0.25
                                                       
-    A_dv_param = {'v_min': A_min, 'v_max': A_max + 0.1*A_step, 'N': None, 'step': A_step, 'round': 1}        
+    A_dv_param = {'v_min': A_min, 'v_max': A_max + 0.1*A_step, 'N': None, 'step': A_step, 'round': 2}        
     A_lr_param = A_dv_param.copy()    
                                             
-    grid_param = {('A_dv', 'A_lr', 'T') : (A_dv_param, A_lr_param)}
+    grid_param = {('A_dv', 'A_lr') : (A_dv_param, A_lr_param)}
     
     PG = ParameterGrid(param, grid_param)
         
@@ -126,7 +122,54 @@ def sim_coil_A(N_worker,
     exper_spec = 'CCE'
     
     h5 = sim_exp_wrapper(simulate,
+        save_raw_data,                  
+        ['x', 'theta', 'Omega', 'sigma', 'w'],
+        ['Omega', 'sigma'],        
+        N_worker,
+        PG,
+        CoilingExperiment.continuous_coiling,
+        h5_filepath, log_dir, sim_dir,
+        exper_spec, overwrite, debug)
+
+    # Analyse raw data and save results
+    compute_roll_frequency_from_euler_angle(h5, PG)
+    
+    return
+
+def sim_coil_f(N_worker,
+        simulate = True,
+        save_raw_data = True,
+        overwrite = False,
+        debug = False):
+    
+    '''
+    Parameter sweep over muscle frequency
+    '''
+    
+    param = continuous_coil_parameter()    
+    param['T'] = 20.0
+    
+    # Parameter Grid    
+    f_min, f_max, f_step = 1.0, 8.01, 0.5
+                                                      
+    f_dv_param = {'v_min': f_min, 'v_max': f_max + 0.1*f_step, 'N': None, 'step': f_step, 'round': 1}        
+    f_lr_param = f_dv_param.copy()    
+                                            
+    grid_param = {('f_dv', 'f_lr') : (f_dv_param, f_lr_param)}
+    
+    PG = ParameterGrid(param, grid_param)
+        
+    filename = (f'continuous_coil_f_min={f_min}_f_max={f_max}_f_step={f_step}_'
+        f'A={param["A_dv"]}_s0_lr={param["s0_lr_t"]}_'
+        f'N={param["N"]}_dt={param["dt"]}.h5')        
+    
+    h5_filepath = sweep_dir / filename
+    exper_spec = 'CCE'
+    
+    h5 = sim_exp_wrapper(simulate,
         save_raw_data,       
+        ['x', 'theta', 'Omega', 'sigma', 'w'],
+        ['Omega', 'sigma'],        
         N_worker,
         PG,
         CoilingExperiment.continuous_coiling,
@@ -142,13 +185,52 @@ def sim_coil_A(N_worker,
     
     return
 
-def sim_coil_f():
+def sim_coil_s0_t(N_worker,
+        simulate = True,
+        save_raw_data = True,
+        overwrite = False,
+        debug = False):
     
-    pass
+    '''
+    Parameter sweep over left-right muscle cut off point
+    '''
+    
+    param = continuous_coil_parameter()    
+    param['T'] = 20.0
+    
+    # Parameter Grid    
+    s0_lr_t_min, s0_lr_t_max, s0_lr_t_step = 0.1, 0.5, 0.05
+                                                      
+    s0_lr_t_param = {'v_min': s0_lr_t_min, 'v_max': s0_lr_t_max + 0.1*s0_lr_t_step, 'N': None, 'step': s0_lr_t_step, 'round': 2}        
+                                            
+    grid_param = {'s0_lr_t' : s0_lr_t_param}
+    
+    PG = ParameterGrid(param, grid_param)
+        
+    filename = (f'continuous_coil_'
+        f's0_lr_min={s0_lr_t_min}_s0_lr_max={s0_lr_t_max}_s0_lr_step={s0_lr_t_step}_'
+        f'A={param["A_dv"]}_f={param["f_dv"]}_'
+        f'N={param["N"]}_dt={param["dt"]}.h5')        
+    
+    h5_filepath = sweep_dir / filename
+    exper_spec = 'CCE'
+    
+    h5 = sim_exp_wrapper(simulate,
+        save_raw_data,       
+        ['x', 'theta', 'Omega', 'sigma', 'w'],
+        ['Omega', 'sigma'],                
+        N_worker,
+        PG,
+        CoilingExperiment.continuous_coiling,
+        h5_filepath,
+        log_dir,
+        sim_dir,
+        exper_spec,
+        overwrite,
+        debug)
 
-def sim_coil_s0_t():
-    
-    pass
+    # Analyse raw data and save results
+    compute_roll_frequency_from_euler_angle(h5, PG)
 
 def sim_E():
     
@@ -163,10 +245,18 @@ def sim_mu():
     pass
 
     
+if __name__ == '__main__':
 
+    N_worker = 12
 
+    # sim_coil_A(N_worker, simulate = True, save_raw_data = True,
+    #     overwrite = False, debug = False)
+    #
+    # sim_coil_f(N_worker, simulate = True, save_raw_data = True,
+    #     overwrite = False, debug = False)
 
-
+    sim_coil_s0_t(N_worker, simulate = True, save_raw_data = True,
+        overwrite = False, debug = False)
 
 
 
--- forward_worm/coil/continuous_coil_plot.py
+++ forward_worm/coil/continuous_coil_plot.py
@@ -3,3 +3,140 @@ Created on 10 Feb 2023
 
 @author: lukas
 '''
+# third-party imports
+import h5py
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+# local imports
+from forward_worm.coil.coil_dirs import log_dir, sim_dir, sweep_dir, video_dir, fig_dir
+from parameter_scan import ParameterGrid
+from simple_worm_experiments.worm_studio import WormStudio
+
+
+def load_h5_file(filename):
+
+    h5_filepath = sweep_dir / filename
+    h5 = h5py.File(str(h5_filepath), 'r')    
+    PG_filepath = log_dir / h5.attrs['grid_filename']
+    PG = ParameterGrid.init_pg_from_filepath(str(PG_filepath))
+
+    return h5, PG
+
+#------------------------------------------------------------------------------ 
+# Generate videos
+
+def generate_worm_videos(h5_filename):
+
+    h5_filepath = sweep_dir / h5_filename
+    h5 = h5py.File(str(h5_filepath), 'r')    
+    PG_filepath = log_dir / h5.attrs['grid_filename']
+    PG = ParameterGrid.init_pg_from_filepath(str(PG_filepath))
+
+    vid_filenames = [f'A_dv={p["A_dv"]}_A_lr={p["A_lr"]}_'
+    f'lam_dv={p["lam_dv"]}_lam_lr={p["lam_lr"]}_'
+    f'f_dv={p["f_dv"]}_f_lr={p["f_lr"]}_s0_lr={p["s0_lr_t"]}_'
+    f'_mu_{p["mu"]:.2f}_N_{p["N"]}_dt_{p["dt"]}' for p in PG.param_arr]
+
+    output_dir = video_dir / Path(h5_filename).stem
+    if not output_dir.exists(): output_dir.mkdir()
+                                                                                             
+    WormStudio.generate_worm_clips_from_PG(
+        PG, vid_filenames, output_dir, sim_dir, 
+        add_trajectory = False,
+        draw_e3 = False,
+        n_arrows = 0.2)
+
+    return
+
+#------------------------------------------------------------------------------ 
+# Plot angle
+
+def plot_roll_frequency_kinematic_parameter(
+        h5_filepath_A, 
+        h5_filepath_f,
+        h5_filepath_s0_t,
+        show = False):
+    '''
+    Plot roll frequency as a function of the kinematic frequency, 
+    muscle amplitude, muscle frequency, 
+    
+    :param h5_filepath_A (str): 
+    :param h5_filepath_f (str):
+    :param h5_filepath_s0_t (str):    
+    :param show (bool):
+    '''
+    
+    h5_A, PG_A = load_h5_file(h5_filepath_A)
+    h5_f, PG_f = load_h5_file(h5_filepath_f)
+    h5_s0_t, PG_s0_t = load_h5_file(h5_filepath_s0_t)
+
+    A_arr = PG_A.v_from_key('A_dv')     
+    f_muscle_A = PG_A.base_parameter['f_dv']
+    f_muscle_arr = PG_f.v_from_key('f_dv')
+    s0_t_arr = PG_s0_t.v_from_key('s0_lr_t')
+    f_muscle_s0_t = PG_s0_t.base_parameter['f_dv']
+
+    f_arr_A = h5_A['f_R_avg'][:] 
+    f_arr_f = h5_f['f_R_avg'][:]
+    f_arr_s0_t = h5_s0_t['f_R_avg'][:]
+        
+    fig = plt.figure(figsize=(12, 8))
+    
+    gs = plt.GridSpec(2, 3)
+    
+    ax00 = plt.subplot(gs[0,0])
+    ax01 = plt.subplot(gs[0,1])
+    ax02 = plt.subplot(gs[0,2])
+    ax10 = plt.subplot(gs[1,0])
+    ax11 = plt.subplot(gs[1,1])
+    ax12 = plt.subplot(gs[1,2])
+                
+    ax00.plot(A_arr, f_arr_A, '-o')
+    ax01.plot(f_muscle_arr, f_arr_f, '-o')
+    ax02.plot(s0_t_arr, f_arr_s0_t, '-o')
+
+    ax10.plot(A_arr, f_arr_A / f_muscle_A, '-o')
+    ax11.plot(f_muscle_arr, f_arr_f / f_muscle_arr, '-o')
+    ax12.plot(s0_t_arr, f_arr_s0_t / f_muscle_s0_t, '-o')
+        
+    lfz = 20
+    
+    ax00.set_ylabel('$f_\mathrm{R}$', fontsize = lfz)    
+    ax10.set_ylabel('$f_\mathrm{R} / f_\mathrm{M}$', fontsize = lfz)
+    ax10.set_xlabel('$A$', fontsize = lfz)
+    ax11.set_xlabel('$f_\mathrm{M}$ [Hz]', fontsize = lfz)
+    ax12.set_xlabel('$s_0$', fontsize = lfz)
+        
+    plt.tight_layout()
+    
+    if show: plt.show()
+
+    filename = 'roll_frequency_A_f_s0.png'
+    plt.savefig(fig_dir / filename)
+    
+    return
+
+if __name__ == '__main__':
+    
+    
+#------------------------------------------------------------------------------ 
+# Plots
+   
+    plot_roll_frequency_kinematic_parameter(
+'continuous_coil_A_min=1.0_A_max=4.01_A_step=0.25_f=5.0_s0_lr=0.25_N=100_dt=0.01.h5',        
+'continuous_coil_f_min=1.0_f_max=8.01_f_step=0.5_A=4.0_s0_lr=0.25_N=100_dt=0.01.h5',
+'continuous_coil_s0_lr_min=0.1_s0_lr_max=0.5_s0_lr_step=0.01_A=4.0_f=5.0_N=100_dt=0.01.h5',        
+    show = True)        
+
+#------------------------------------------------------------------------------ 
+# Videos
+
+#     generate_worm_videos(
+# 'continuous_coil_A_min=1.0_A_max=4.01_A_step=1.0_f=5.0_s0_lr=0.25_N=100_dt=0.01.h5')
+
+#     generate_worm_videos(
+# 'continuous_coil_s0_lr_min=0.1_s0_lr_max=0.5_s0_lr_step=0.05_A=4.0_f=5.0_N=100_dt=0.01.h5')
+
+        
+    print('Finished')
